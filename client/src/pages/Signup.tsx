e9efe4ca896235b2b8b29f8822df8bace070d7a5
Kinamd-east <Kinamdeast@gmail.com>
2025-07-31 14:37:45 +0100

    Extra changes

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 import { toast } from "sonner";
+import { Card, CardContent } from "@/components/ui/card";
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -53,36 +57,55 @@ const Signup = () => {
   };
 
   return (
-    <form onSubmit={handleSignup}>
-      <input
-        type="text"
-        name="username"
-        placeholder="Username"
-        value={form.username}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={form.email}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={form.password}
-        onChange={handleChange}
-        required
-      />
-      <button type="submit" disabled={loading} className="disabled:bg-gray-400">
-        {loading ? "Please Wait" : "Sign up"}
-      </button>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-    </form>
+    <div className="min-h-screen flex items-center justify-center bg-white px-4">
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7, ease: "easeOut" }}
+        className="w-full max-w-sm"
+      >
+        <Card className="bg-black/4 backdrop-blur-md shadow-2xl border-white/10 text-black">
+          <CardContent className="p-6">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Create your account
+            </h2>
+
+            <form onSubmit={handleSignup}>
+              <Input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={form.username}
+                onChange={handleChange}
+                required
+              />
+              <Input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={form.email}
+                onChange={handleChange}
+                className="bg-white/5 text-black placeholder-white/90 border-white/20"
+                required
+              />
+              <Input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={form.password}
+                onChange={handleChange}
+                className="bg-white/5 text-black placeholder-white/90 border-white/20"
+                required
+              />
+              <Button type="submit" className="w-full mt-2 cursor-pointer">
+                {loading ? "Please wait..." : "Sign up"}
+              </Button>
+              {error && <p style={{ color: "red" }}>{error}</p>}
+            </form>
+          </CardContent>
+        </Card>
+      </motion.div>
+    </div>
   );
 };
 
